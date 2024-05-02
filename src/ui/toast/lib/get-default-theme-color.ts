cfa7e71446eea336bdfbf445d999c64f27b5154f
Aleksandr D. Savchenko <asavchenko@datatech.ru>
2024-05-02 15:09:23 +0300

    Refactoring 2 1. Удалить toast 2. Починить переводы при успешном логине

--- src/ui/toast/lib/get-default-theme-color.ts
+++
@@ -1,3 +0,0 @@
-export function getDefaultThemeColor(): string {
-  return '#dfdfc9'
-}
--- src/ui/toast/lib/get-theme-color.ts
+++
@@ -1,6 +0,0 @@
-import { getThemeColorMeta } from './get-theme-color-meta'
-
-export function getThemeColor(): string {
-  // TODO когда появится темы надо сюда bg подставить по умолчанию
-  return getThemeColorMeta().content
-}
